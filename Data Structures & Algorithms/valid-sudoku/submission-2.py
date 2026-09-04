b00a6054e7de2834a98098b9d225ebbe79b7819a
THYAGARAJAN R <42999765+THYAGARAJAN786@users.noreply.github.com>
2026-09-04 18:19:49 +0530

    Add: valid-sudoku - submission-2

---
+++ Data Structures & Algorithms/valid-sudoku/submission-2.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row_set_list = [set() for i in range(9)]
+        col_set_list  = [set() for i in range(9)]
+        square_set_list = [set() for i in range(9)]
+        for row, rows in enumerate(board):
+            for col, elem in enumerate(rows):
+                if board[row][col] == '.':
+                    continue
+                #end if
+                square_index = (row //3) * 3 + (col//3)
+                if elem in row_set_list[row] or\
+                   elem in col_set_list[col] or \
+                   elem in square_set_list[square_index]:\
+                    return False
+                row_set_list[row].add(board[row][col])
+                col_set_list[col].add(board[row][col])
+                square_set_list[square_index].add(board[row][col])
+            #end for
+        #end for
+        return True
